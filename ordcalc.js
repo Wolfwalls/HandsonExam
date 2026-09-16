d0474079ca1f006b71c44b5341fbe635075a5104
Wolfwall <djh09511@mail.com>
2026-09-16 13:15:48 -0400

    FIrst project init commit

---
+++ ordcalc.js
@@ -0,0 +1,26 @@
+const itemprice= document.getElementById("price")
+const itemamount= document.getElementById("quantity")
+const calcbutton = document.getElementById("calculate")
+const pretotal = document.getElementById("lowtotal")
+const subtratotal = document.getElementById("disctotal")
+const lastotal = document.getElementById("fintotal")
+
+
+let subtotal = 0;
+let total=0;
+
+calcbutton.onclick = function(){
+    
+    subtotal = itemprice.value * itemamount.value;
+    if(subtotal >= 50){
+        total = subtotal * .90;
+        discountotal = subtotal - total
+    } 
+    else{
+        total = subtotal
+        discountotal = 0
+    }
+    pretotal.innerText = subtotal;
+    subtratotal.innerText = discountotal;
+    lastotal.innerText = total;
+}
